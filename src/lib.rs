cfb5cf6bdf608eaba9be1d1f5b7e6487c56406d4
slogemann1 <sllogemann1@gmail.com>
2020-12-25 02:29:12 +0100

    Initial Commit, created and documented response module along with a preliminary error type

---
+++ src/lib.rs
@@ -0,0 +1,98 @@
+//TODO: Add response module, add examples, add tests, test code
+#![deny( //These lint options are copied from https://pascalhertleif.de/artikel/good-practices-for-writing-rust-libraries/
+    missing_docs,
+    missing_debug_implementations, missing_copy_implementations,
+    trivial_casts, trivial_numeric_casts,
+    unsafe_code,
+    unstable_features,
+    unused_import_braces,
+    unused_qualifications
+)]
+
+//! # Datamuse Api Wrapper
+//! This library provides an unofficial wrapper around the Datamuse api for use in rust projects. 
+//! The Datamuse api allows users to retrieve lists of words from different vocabulary lists
+//! based on a large number of different parameters. Additionally, it can be used to retrieve
+//! words based on a short string of letters, allowing for autocomplete functionality.
+//! Currently, the api does not require an api key and can be used to make up to 100,000
+//! requests per day after which the requests may be rate-limited (for higher usage see website).
+//! If you use this api you shuold make reference to the Datamuse api in your project.
+//! For more information see the official documentation at [https://www.datamuse.com/api/](https://www.datamuse.com/api/)
+
+extern crate reqwest;
+//extern crate serde_json;
+
+//#[macro_use]
+//extern crate serde;
+
+use std::fmt::{ self, Display, Formatter };
+use std::error;
+use std::result;
+
+mod request;
+//mod response;
+
+pub use request::*;
+
+/// This struct represents the client which can be used to make requests
+/// to the Datamuse api. Requests can be created using the new_query() method
+#[derive(Debug)]
+pub struct DatamuseClient {
+    client: reqwest::Client
+}
+
+impl DatamuseClient {
+    /// Returns a new DatamuseClient struct
+    pub fn new() -> Self {
+        DatamuseClient {
+            client: reqwest::Client::new()
+        }
+    }
+
+    /// Returns a new [RequestBuilder](request::RequestBuilder) struct with which requests can be created
+    /// and later sent. As parameters the vocabulary set and endpoint of the request are required. See
+    /// their individual documentations for more information
+    pub fn new_query<'a>(&'a mut self, vocabulary: Vocabulary, endpoint: EndPoint) -> RequestBuilder<'a> {
+        RequestBuilder::new(self, vocabulary, endpoint)
+    }
+}
+
+/// A type alias for Results with the library Error type
+pub type Result<T> = result::Result<T, Error>;
+
+/// An enum representing the different kind of Errors that can be returned within the library
+#[derive(Debug)]
+pub enum Error {
+    /// An error resulting from an underlying call to reqwest
+    ReqwestError(reqwest::Error),
+    /// An error resulting from the use of a parameter not availible for a specific vocabulary list
+    VocabularyError((String, String)),
+    /// An error resulting from the use of a parameter not intended for the specified endpoint
+    EndPointError((String, String))
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::ReqwestError(err) => write!(f, "{}", err),
+            Self::VocabularyError((lang, param)) => write!(f, "Error: The parameter {} is not yet supported for {}", param, lang),
+            Self::EndPointError((endpoint, param)) => write!(f, "Error: The parameter {} is not supported for {}", param, endpoint)
+        }
+    }
+}
+
+impl error::Error for Error {}
+
+impl From<reqwest::Error> for Error {
+    fn from(error: reqwest::Error) -> Self {
+        Error::ReqwestError(error)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
---
+++ src/request.rs
@@ -0,0 +1,471 @@
+use std::fmt::{ self, Display, Formatter };
+use reqwest;
+use crate::{ DatamuseClient, Error, Result };
+
+/// Use this struct to build requests to send to the Datamuse api.
+/// This request can be sent either by building it into a Request with build()
+/// and then using the send() method on the resulting Request or using send() to 
+/// send it directly. Note that not all parameters can be used for each vocabulary
+/// and endpoint
+#[derive(Debug)]
+pub struct RequestBuilder<'a> {
+    client: &'a mut DatamuseClient,
+    endpoint: EndPoint,
+    vocabulary: Vocabulary,
+    parameters: Vec<Parameter>,
+    topics: Vec<String>, //Makes adding topics make easier, later added to parameters
+    meta_data_flags: Vec<MetaDataFlag> //Same issue as topics
+}
+
+/// This struct represents a built request that can be sent using the send() method
+#[derive(Debug)]
+pub struct Request {
+    request: reqwest::Request
+}
+
+/// This enum represents the different endpoints of the Datamuse api.
+/// The "words" endpoint returns word lists based on a set of parameters,
+/// whereas the "suggest" endpoint returns suggestions for words based on a
+/// hint string (autocomplete).
+/// For more detailed information visit the [Datamuse website](https://www.datamuse.com/api/)
+#[derive(Clone, Copy, Debug)]
+pub enum EndPoint {
+    /// The "words" endpoint (the official endpoint is also "/words")
+    Words,
+    /// The "suggest" endpoint (the official endpoint is "/sug")
+    Suggest
+}
+
+/// This enum represents the different vocabulary lists which can be used as
+/// a source for the requests. There are currently two language options
+/// (English or Spanish) and an alternative English option from wikipedia.
+/// For more detailed information visit the [Datamuse website](https://www.datamuse.com/api/)
+#[derive(Clone, Copy, Debug)]
+pub enum Vocabulary {
+    /// The default vocabulary list with 550,000 words
+    English,
+    /// The Spanish vocabulary list with 500,000 words
+    Spanish,
+    /// The alternative English vocabulary list with 6 million words
+    EnglishWiki
+}
+
+/// This enum represents the different possibilites the "Related" parameter can take.
+/// These parameters can be combined in any possible configuration, although very specific
+/// queries can limit results. Each option is shortly explained below.
+/// For more detailed information for each type visit the [Datamuse website](https://www.datamuse.com/api/)
+#[derive(Clone, Copy, Debug)]
+pub enum RelatedType {
+    /// This parameter returns nouns that are typically modified by the given adjective
+    NounModifiedBy,
+    /// This parameter returns adjectives that typically modify by the given noun
+    AdjectiveModifier,
+    /// This parameter returns synonyms for the given word
+    Synonym,
+    /// This parameter returns associated words for the given word
+    Trigger,
+    /// This parameter returns antonyms for the given word
+    Antonym,
+    /// This parameter returns the kind of which a more generalized word is
+    KindOf,
+    /// This parameter returns a more specific kind the given category word (opposite of KindOf)
+    MoreGeneral,
+    /// This parameter returns words that describe things which the given word is comprised of
+    Comprises,
+    /// This parameter returns words that describe things which the given word is a part of (opposite of Comprises)
+    PartOf,
+    /// This parameter returns words that are typically found after the given word
+    Follower,
+    /// This parameter returns words that are typically found before the given word
+    Predecessor,
+    /// This parameter returns words that rhyme with the given word
+    Rhyme,
+    /// This parameter returns words that almost rhyme with the given word
+    ApproximateRhyme,
+    /// This parameter returns words that sound like the given word
+    Homophones,
+    /// This parameter returns words which have matching consonants but differing vowels from the given word
+    ConsonantMatch
+}
+
+/// This enum represents the various flags which can be set for retrieving metadata for each word.
+/// These metadata flags can be combined in any manner. Each is shortly described below
+#[derive(Clone, Copy, Debug)]
+pub enum MetaDataFlag {
+    /// Provides definitions for each of the words in the response
+    Definitions,
+    /// Provides what type of speech each of the words in the response is
+    PartsOfSpeech,
+    /// Provides how many syllables each of the words in the response has
+    SyllableCount,
+    /// Provides pronunciations for each of the words in the response based on
+    /// the given pronunciation format
+    Pronunciation(PronunciationFormat),
+    /// Provides how frequently each of the words in the response is found
+    WordFrequency
+}
+
+/// This enum represents the ways pronunciations returned by the "Pronunciation" metadata flag
+/// can be given
+#[derive(Clone, Copy, Debug)]
+pub enum PronunciationFormat {
+    /// The [ARPABET](https://en.wikipedia.org/wiki/ARPABET) pronunciation format
+    Arpabet,
+    /// The [International Phonetic Alphabet](https://en.wikipedia.org/wiki/International_Phonetic_Alphabet) pronunciation format
+    Ipa
+}
+
+#[derive(Clone, Debug)]
+struct RelatedTypeHolder {
+    related_type: RelatedType,
+    value: String
+}
+
+#[derive(Clone, Debug)]
+enum Parameter {
+    MeansLike(String),
+    SoundsLike(String),
+    SpelledLike(String),
+    Related(RelatedTypeHolder),
+    Topics(Vec<String>),
+    LeftContext(String),
+    RightContext(String),
+    MaxResults(u16), //Also supported for sug endpoint
+    MetaData(Vec<MetaDataFlag>),
+    HintString(String) //Only supported for sug endpoint
+}
+
+impl<'a> RequestBuilder<'a> {
+    /// Sets a query parameter for words which have a similar meaning to the given word
+    pub fn means_like(mut self, word: &str) -> Self {
+        self.parameters.push(
+            Parameter::MeansLike(
+                String::from(word)
+            )
+        );
+
+        self
+    }
+
+    /// Sets a query parameter for words which sound similar to the given word
+    pub fn sounds_like(mut self, word: &str) -> Self {
+        self.parameters.push(
+            Parameter::SoundsLike(
+                String::from(word)
+            )
+        );
+
+        self
+    }
+
+    /// Sets a query parameter for words which have a similar spelling to the given word.
+    /// This parameter allows for wildcard charcters with '?' matching a single letter and
+    /// '*' matching any number of letters
+    pub fn spelled_like(mut self, word: &str) -> Self {
+        self.parameters.push(
+            Parameter::SpelledLike(
+                String::from(word)
+            )
+        );
+
+        self
+    }
+
+    /// Sets a query parameter for words which are related to the given word.
+    /// The various options for relations are given in the [RelatedType](RelatedType) enum.
+    /// See its documentation for more information on the options.
+    /// Note that this is currently **not available** for the Spanish vocabulary set
+    pub fn related(mut self, rel_type: RelatedType, word: &str) -> Self {
+        self.parameters.push(
+            Parameter::Related(
+                RelatedTypeHolder {
+                    related_type: rel_type,
+                    value: String::from(word)
+                }
+            )
+        );
+
+        self
+    }
+
+    /// Sets a query parameter for words which fall under the topic of the given word.
+    /// Multiple topics can be specified at once, however requests are limited to five
+    /// topics and as such any specified over this limit will be ignored
+    pub fn add_topic(mut self, word: &str) -> Self {
+        self.topics.push(
+            String::from(word)
+        );
+
+        self
+    }
+
+    /// Sets a query parameter to refer to the word directly before the main query term
+    pub fn left_context(mut self, word: &str) -> Self {
+        self.parameters.push(
+            Parameter::LeftContext(
+                String::from(word)
+            )
+        );
+
+        self
+    }
+
+    /// Sets a query parameter to refer to the word directly after the main query term
+    pub fn right_context(mut self, word: &str) -> Self {
+        self.parameters.push(
+            Parameter::RightContext(
+                String::from(word)
+            )
+        );
+
+        self
+    }
+
+    /// The maximum number of results that should be returned. By default this is set to 100
+    /// and it can be increased to a maximum of 1000. This parameter is also **allowed** for the
+    /// "suggest" endpoint
+    pub fn max_results(mut self, maximum: u16) -> Self {
+        self.parameters.push(
+            Parameter::MaxResults(
+                maximum
+            )
+        );
+
+        self
+    }
+
+    /// Sets a metadata flag to specify data returned with each word.
+    /// The various options for flags are given in the [MetaDataFlag](MetaDataFlag) enum.
+    /// See its documentation for more information on the options
+    pub fn meta_data(mut self, flag: MetaDataFlag) -> Self {
+        self.meta_data_flags.push(flag);
+
+        self
+    }
+
+    /// Sets the hint string for the "suggest" endpoint. Note that this is
+    /// **not allowed** for the "words" endpoint
+    pub fn hint_string(mut self, hint: &str) -> Self {
+        self.parameters.push(
+            Parameter::HintString(
+                String::from(hint)
+            )
+        );
+
+        self
+    }
+
+    /// Converts the RequestBuilder into a Request which can be executed by calling the send()
+    /// method on it. This method will return an error if any of the given parameters have not been
+    /// used correctly or the underlying call to reqwest to build the request fails
+    pub fn build(&self) -> Result<Request> {
+        let mut params_list: Vec<(String, String)> = Vec::new();
+        let mut parameters = self.parameters.clone();
+
+        if self.topics.len() > 0 {
+            parameters.push(
+                Parameter::Topics(self.topics.clone())
+            );
+        }
+
+        if self.meta_data_flags.len() > 0 {
+            parameters.push(
+                Parameter::MetaData(self.meta_data_flags.clone())
+            );
+
+            for flag in self.meta_data_flags.clone() {
+                if let MetaDataFlag::Pronunciation(PronunciationFormat::Ipa) = flag {
+                    params_list.push((String::from("ipa"), 1.to_string()));
+                }
+            }
+        }
+        
+        let vocab_params = self.vocabulary.build();
+        if let Some(val) = vocab_params {
+            params_list.push(val);
+        }
+
+        for param in parameters {
+            params_list.push(
+                param.build(&self.vocabulary, &self.endpoint)?
+            );
+        }
+
+        let request = self.client.client.get(&format!("https://api.datamuse.com/{}", self.endpoint.get_string()))
+            .query(&params_list)
+            .build()?;
+
+        Ok(Request {
+            request
+        })
+    }
+
+    pub(crate) fn new(client: &'a mut DatamuseClient, vocabulary: Vocabulary, endpoint: EndPoint) -> Self {
+        RequestBuilder {
+            client,
+            endpoint,
+            vocabulary,
+            parameters: Vec::new(),
+            topics: Vec::new(),
+            meta_data_flags: Vec::new()
+        }
+    }
+}
+
+impl Parameter {
+    fn build(&self, vocab: &Vocabulary, endpoint: &EndPoint) -> Result<(String, String)> {
+        if let Parameter::Related(_) = self { //Error for using related with spanish vocabulary
+            if let Vocabulary::Spanish = vocab {
+                return Err(
+                    Error::VocabularyError((
+                        String::from("Spanish"), String::from("Related")
+                    ))
+                );
+            }
+        }
+
+        if let EndPoint::Words = endpoint { //Error for using hint string for the words endpoint
+            if let Parameter::HintString(_) = self {
+                return Err(
+                    Error::EndPointError((
+                        String::from("Words"), String::from("HintString")
+                    ))
+                );
+            }
+        }
+
+        if let EndPoint::Suggest = endpoint {
+            match self {
+                Parameter::MaxResults(_) => (),
+                Parameter::HintString(_) => (),
+                val => {
+                    return Err(
+                        Error::EndPointError((
+                            String::from("Suggest"), format!("{}", val)
+                        ))
+                    );
+                }
+            }
+        }
+
+        let param = match self {
+            Self::MeansLike(val) => (String::from("ml"), val.clone()),
+            Self::SoundsLike(val) => (String::from("sl"), val.clone()),
+            Self::SpelledLike(val) => (String::from("sp"), val.clone()),
+            Self::Related(val) => {
+                (
+                    format!("rel_{}", val.get_type_identifier()),
+                    val.get_word()
+                )
+            },
+            Self::Topics(topic_list) => {
+                let mut topics_concat = String::from("");
+                let mut len = topic_list.len();
+
+                if len > 5 {
+                    len = 5;
+                } 
+
+                let mut i = 0;
+                while i < len - 1 {
+                    topics_concat = topics_concat + &topic_list[i];
+                    topics_concat.push(',');
+                    i += 1;
+                }
+                topics_concat = topics_concat + &topic_list[len - 1];
+
+                (String::from("topics"), topics_concat)
+            },
+            Self::LeftContext(val) => (String::from("lc"), val.clone()),
+            Self::RightContext(val) => (String::from("rc"), val.clone()),
+            Self::MaxResults(val) => (String::from("max"), val.to_string()),
+            Self::MetaData(flags) => {
+                let mut flags_concat = String::from("");
+                for flag in flags {
+                    flags_concat.push(flag.get_letter_identifier());
+                }
+
+                (String::from("md"), flags_concat)
+            },
+            Self::HintString(val) => (String::from("s"), val.clone())
+        };
+
+        Ok(param)
+    }
+}
+
+impl Display for Parameter {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let name = match self {
+            Self::MeansLike(_) => "MeansLike",
+            Self::SoundsLike(_) => "SoundsLike",
+            Self::SpelledLike(_) => "SpelledLike",
+            Self::Related(_) => "Related",
+            Self::Topics(_) => "Topic",
+            Self::LeftContext(_) => "LeftContext",
+            Self::RightContext(_) => "RightContext",
+            Self::MaxResults(_) => "MaxResults",
+            Self::MetaData(_) => "MetaData",
+            Self::HintString(_) => "HintString"
+        };
+
+        write!(f, "{}", name)
+    }
+}
+
+impl RelatedTypeHolder {
+    fn get_type_identifier(&self) -> String {
+        match self.related_type {
+            RelatedType::NounModifiedBy => String::from("jja"),
+            RelatedType::AdjectiveModifier => String::from("jjb"),
+            RelatedType::Synonym => String::from("syn"),
+            RelatedType::Trigger => String::from("trg"),
+            RelatedType::Antonym => String::from("ant"),
+            RelatedType::KindOf => String::from("spc"),
+            RelatedType::MoreGeneral => String::from("gen"),
+            RelatedType::Comprises => String::from("com"),
+            RelatedType::PartOf => String::from("par"),
+            RelatedType::Follower => String::from("bga"),
+            RelatedType::Predecessor => String::from("bgb"),
+            RelatedType::Rhyme => String::from("rhy"),
+            RelatedType::ApproximateRhyme => String::from("nhy"),
+            RelatedType::Homophones => String::from("hom"),
+            RelatedType::ConsonantMatch => String::from("cns")
+        }
+    }
+
+    fn get_word(&self) -> String {
+        self.value.clone()
+    }
+}
+
+impl MetaDataFlag {
+    fn get_letter_identifier(&self) -> char {
+        match self {
+            Self::Definitions => 'd',
+            Self::PartsOfSpeech => 'p',
+            Self::SyllableCount => 's',
+            Self::Pronunciation(_) => 'r',
+            Self::WordFrequency => 'f'
+        }
+    }
+}
+
+impl EndPoint {
+    fn get_string(&self) -> String {
+        match self {
+            Self::Words => String::from("words"),
+            Self::Suggest => String::from("sug")
+        }
+    }
+}
+
+impl Vocabulary {
+    fn build(&self) -> Option<(String, String)> {
+        match self {
+            Vocabulary::Spanish => Some((String::from("v"), String::from("es"))),
+            Vocabulary::EnglishWiki => Some((String::from("v"), String::from("enwiki"))),
+            Vocabulary::English => None
+        }
+    }
+}
